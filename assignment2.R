34f5f98ee974bb9ca4e1b637341f2a96a21486ca
krishnatheja36 <ktheja@gmail.com>
2015-06-21 16:46:11 -0400

    Update assignment2.R

--- assignment2.R
+++ assignment2.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix())
 	setmatrix <- function(inverse) i <<- inverse
 	getmatrix <- function() i
  	
-	list(set=set, get=get, setmatrix=setmatrix, getmatrix =getmatrix)
+	list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 cacheSolve <- function(x, ...)
@@ -22,7 +22,7 @@ cacheSolve <- function(x, ...)
 		message("getting cached data")
 		return(i)
 	}	
-	message("caching the data") #displays message when data is being cached ie. only during the first run.
+	message("caching the data") #displays a message when data is being cached ie. only during the first run.
 	data <- x$get()
 	i <- solve(data, ...)
 	x$setmatrix(i)
